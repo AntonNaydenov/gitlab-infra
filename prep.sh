93ea4b60bebc75f01eb3b1502a3caed870ea44c5
Анон <57961790+AntonNaydenov@users.noreply.github.com>
2022-12-07 13:16:43 +0300

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -2,7 +2,7 @@
 export ipaddr=$(hostname -i)
 mkdir -p /srv/gitlab/{config,logs,data}
 docker compose build --no-cache
-mkdit -pv ./hidden
+mkdir -pv ./hidden
 chmod 700 ./hidden
 docker compose up -d
 cat ./hidden/hostname
