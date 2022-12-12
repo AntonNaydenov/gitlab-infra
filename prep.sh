e203b0db48dc7a25eafa4ba7fd58eed66f1c391f
Анон <57961790+AntonNaydenov@users.noreply.github.com>
2022-12-12 10:03:15 +0300

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -5,6 +5,7 @@ docker compose build --no-cache
 mkdir -pv ./hidden
 mkdir -pv ./configs
 chmod 700 ./hidden
+cp ./torrc ./configs/torrc
 chmod 700 ./configs
 docker compose up -d
 cat ./hidden/hostname
